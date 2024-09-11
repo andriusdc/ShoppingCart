9138b8412308c8d9b566dbcaa8ff0bc63b1509c2
Andrius <andriusdomik@hotmail.com>
2024-09-11 12:55:36 -0300

    feat:add Cart level models and tests

--- shopping_cart_case/src/core/domain/models.py
+++ shopping_cart_case/src/core/domain/models.py
@@ -9,9 +9,23 @@ class User():
         self.role = role
         
 class Product:
-    def __init__(self, product_id: int, product_name: str, description: str, price: int, created_at: datetime = None):
+    def __init__(self, product_id: int, product_name: str, description: str, price: int, created_at: datetime = None) -> None:
         self.product_id = product_id
         self.product_name = product_name
         self.description = description
         self.price = price
         self.created_at = created_at or datetime.now()
+
+class Cart:
+    def __init__(self, cart_id: int, user_id: int, created_at: datetime = None) -> None:
+        self.cart_id = cart_id
+        self.user_id = user_id
+        self.created_at = created_at or datetime.now()
+
+class CartItem:
+    def __init__ (self, cart_item_id: int, cart_id: int, product_id: int, quantity: int, added_at: datetime = None) -> None:
+        self.cart_item_id = cart_item_id
+        self.cart_id = cart_id
+        self.product_id = product_id
+        self.quantity = quantity
+        self.added_at = added_at
--- shopping_cart_case/tests/unit/test_core_models.py
+++ shopping_cart_case/tests/unit/test_core_models.py
@@ -1,6 +1,6 @@
 import pytest
 from datetime import datetime
-from src.core.domain.models import User,Product
+from src.core.domain.models import User,Product,Cart,CartItem
 
 def test_user_model():
     created_at=datetime(2024,9,11,0,0,0)
@@ -21,3 +21,21 @@ def test_product_model():
     assert product.description=="Fruit unit"
     assert product.price==5
     assert product.created_at==created_at
+
+def test_cart_model():
+    created_at=datetime(2024,9,11,0,0,0)
+    cart=Cart(cart_id=1,user_id=1,created_at=created_at)
+
+    assert cart.cart_id==1
+    assert cart.user_id==1
+    assert cart.created_at==created_at
+
+def test_cart_items_model():
+    added_at=datetime(2024,9,11,0,0,0)
+    cartItems=CartItem(cart_item_id=1, cart_id=1, product_id =2, quantity=1,added_at=added_at)
+
+    assert cartItems.cart_item_id == 1
+    assert cartItems.cart_id == 1
+    assert cartItems.product_id == 2
+    assert cartItems.quantity == 1
+    assert cartItems.added_at == added_at
