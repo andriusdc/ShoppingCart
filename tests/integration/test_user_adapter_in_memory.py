287d79af0d3dd76abd6ed25fe4aae7a56a6e4850
Andrius <andriusdomik@hotmail.com>
2024-09-14 17:54:32 -0300

    refactor:add conftest

--- tests/integration/test_user_adapter_in_memory.py
+++ tests/integration/test_user_adapter_in_memory.py
@@ -1,39 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from src.core.domain.models import db, User, app
-from src.core.application.password_service import PasswordService
+from src.core.domain.models import User, db
 from src.adapters.user_adapter import UserAdapter
-
-
-@pytest.fixture
-def test_client():
-    """
-    Fixture for setting up a test client with an in-memory SQLite database.
-
-    Configures the Flask app for testing and sets up an in-memory SQLite database.
-    Provides a test client to be used in tests and ensures the database is cleaned up
-    after each test.
-
-    :return: Flask test client.
-    """
-    app.config["TESTING"] = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
-
-    with app.app_context():
-        db.create_all()
-        yield app.test_client()
-        db.session.remove()
-        db.drop_all()
-
-
-@pytest.fixture
-def password_service():
-    """
-    Fixture for providing an instance of PasswordService.
-
-    :return: PasswordService instance.
-    """
-    return PasswordService()
 
 
 @pytest.fixture
