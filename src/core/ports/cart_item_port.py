9415702b03acd1c9cfd2a09b452e8351a9e9e6ae
Andrius <andriusdomik@hotmail.com>
2024-09-14 21:44:14 -0300

    refactor: implement cart itm port using abstractmethod

--- src/core/ports/cart_item_port.py
+++ src/core/ports/cart_item_port.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
 from typing import Optional, List
 from src.core.domain.models import CartItem
 
 
-class CartItemPort:
+class CartItemPort(ABC):
+    @abstractmethod
     def add_cart_item(self, cart_item: CartItem) -> None:
         """
         Add a new cart item to the database.
 
         :param cart_item: CartItem instance to be added.
         """
-        raise NotImplementedError
+        pass
 
+    @abstractmethod
     def get_cart_item(self, cart_item_id: int) -> Optional[CartItem]:
         """
         Retrieve a cart item by its ID.
@@ -19,8 +22,9 @@ class CartItemPort:
         :param cart_item_id: ID of the cart item to retrieve.
         :return: CartItem instance if found, otherwise None.
         """
-        raise NotImplementedError
+        pass
 
+    @abstractmethod
     def update_cart_item(self, cart_item: CartItem) -> None:
         """
         Update an existing cart item's details in the database.
@@ -28,8 +32,9 @@ class CartItemPort:
         :param cart_item: CartItem instance with updated data.
         :raises ValueError: If the cart item does not exist.
         """
-        raise NotImplementedError
+        pass
 
+    @abstractmethod
     def delete_cart_item(self, cart_item_id: int) -> None:
         """
         Delete a cart item by its ID.
@@ -37,12 +42,13 @@ class CartItemPort:
         :param cart_item_id: ID of the cart item to delete.
         :raises ValueError: If the cart item does not exist.
         """
-        raise NotImplementedError
+        pass
 
+    @abstractmethod
     def list_cart_items(self) -> List[CartItem]:
         """
         List all cart items in the database.
 
         :return: List of CartItem instances.
         """
-        raise NotImplementedError
+        pass
